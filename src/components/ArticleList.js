9878ed6e013fe7959596c5080ca9c2928c3abce2
Alexander Nazarov <nazarov84@gmail.com>
2017-03-03 21:24:17 +0200

    Just fixed an issue with a key

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,7 +3,7 @@ import Article from './Article'
 
 export default (props) => {
     const {articles} = props
-    const articleComponents = articles.map(article => <div><li key={article.id}><Article article={article}/></li><hr /></div>)
+    const articleComponents = articles.map(article => <div key={article.id}><li><Article article={article}/></li><hr /></div>)
 
     return (
         <ul>
