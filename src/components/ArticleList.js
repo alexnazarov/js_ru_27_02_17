dc09787b5c7a2b783dea9b06c60a7b366d0cc433
Alexander Nazarov <nazarov84@gmail.com>
2017-03-08 20:46:00 +0200

    Add decorator for accordion

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,18 +1,15 @@
 import React, {PropTypes, Component} from 'react'
 import Article from './Article'
+import accordion from '../decorators/accordion'
 
-export default class ArticleList extends Component {
-    state = {
-        openArticleId: null
-    }
-
+class ArticleList extends Component {
     render() {
-        const {articles} = this.props
+        const {articles, openArticleId, toggleOpenArticle} = this.props
 
         const articleComponents = articles.map(article => <li key={article.id}>
             <Article article={article}
-                     isOpen={article.id === this.state.openArticleId}
-                     toggleOpen={this.toggleOpenArticle(article.id)}
+                isOpen={article.id === openArticleId}
+                toggleOpen={toggleOpenArticle(article.id)}
             />
         </li>)
 
@@ -22,14 +19,12 @@ export default class ArticleList extends Component {
             </ul>
         )
     }
-
-    toggleOpenArticle = openArticleId => ev => {
-        this.setState({
-            openArticleId
-        })
-    }
 }
 
 ArticleList.propTypes = {
-    articles: PropTypes.array.isRequired
+    articles: PropTypes.array.isRequired,
+    openArticleId: PropTypes.number.isRequired,
+    toggleOpenArticle: PropTypes.func.isRequired
 }
+
+export default accordion(ArticleList)
