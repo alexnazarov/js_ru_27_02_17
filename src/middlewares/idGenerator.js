d7a541d1e0c72dd191d8601404962687a17c9dd9
Roman <kaboom.ua@gmail.com>
2017-03-22 22:57:01 +0200

    Update idGenerator.js

--- src/middlewares/idGenerator.js
+++ src/middlewares/idGenerator.js
@@ -4,8 +4,10 @@ export default store => next => action => {
     const { type, payload } = action
 
     switch(type) {
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
         case ADD_COMMENT:
             const commentsArray = store.getState().comments
+            //очень плохая логика, никакой гарантии уникальности, он же не сортированый по id
             const lastCommentId = commentsArray[commentsArray.length-1].id;
 
             next({
